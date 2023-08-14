c169159ec33ce2b7b59f1692c27590f13c0106ca
no305 <95123539+no305@users.noreply.github.com>
2023-08-14 22:38:35 +0100

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x252df95d8ddf8ce033f049d0319215953ea5c364";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
